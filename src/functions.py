26fe22e8970600702a11be56b56d53223f29d2c3
Maxime <max16150@gmail.com>
2024-01-30 14:20:07 +0100

    wip

--- src/functions.py
+++ src/functions.py
@@ -30,3 +30,25 @@ def removeBiggestElementsOnBinaryImage(image):
     # Apply AND operation
     andOperation = cv2.bitwise_and(dilation, duplicatedImage)
     return andOperation
+
+def dessiner_rectangle_centre(image, ligne, epaisseur, couleur):
+    x1, y1, x2, y2 = ligne
+
+    # Vecteur directeur de la ligne
+    vecteur_ligne = np.array([x2 - x1, y2 - y1])
+    longueur_ligne = np.linalg.norm(vecteur_ligne)
+
+    # Vecteur perpendiculaire à la ligne
+    vecteur_perpendiculaire = np.array([-vecteur_ligne[1], vecteur_ligne[0]])
+    vecteur_perpendiculaire = vecteur_perpendiculaire / np.linalg.norm(vecteur_perpendiculaire) * epaisseur / 2
+
+    # Calculer les coins du rectangle
+    coin1 = np.array([x1, y1]) + vecteur_perpendiculaire
+    coin2 = np.array([x1, y1]) - vecteur_perpendiculaire
+    coin3 = np.array([x2, y2]) - vecteur_perpendiculaire
+    coin4 = np.array([x2, y2]) + vecteur_perpendiculaire
+
+    # Dessiner le rectangle
+    points = np.array([coin1, coin2, coin3, coin4], np.int32)
+    points = points.reshape((-1, 1, 2))
+    cv2.fillPoly(image, [points], color=couleur)
--- src/sample.py
+++ src/sample.py
@@ -6,29 +6,42 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from shapely.geometry import LineString
 
-initialized = False
-SCREEN_IMAGE_SELECTOR_CONTROL = 'Selecteur d\'image'
+ITERATION = ' 2'
 
-SCREEN_HSV_CONTROL = 'Controlleur HSV'
-SCREEN_HSV = 'HSV'
+initialized = False
+SCREEN_IMAGE_SELECTOR_CONTROL = 'Selecteur d\'image' + str(ITERATION)
+
+SCREEN_HSV_CONTROL = 'Controlleur HSV' + str(ITERATION)
+SCREEN_HSV = 'HSV' + str(ITERATION)
 globalHsvResult = None
 
-SCREEN_OPENING_CONTROL = 'Controlleur Ouverture'
-SCREEN_OPENING = 'Ouverture'
+SCREEN_OPENING_CONTROL = 'Controlleur Ouverture' + str(ITERATION)
+SCREEN_OPENING = 'Ouverture' + str(ITERATION)
 globalOpeningResult = None
 
-SCREEN_LINE_DETECTION_CONTROL = 'Controlleur Detection de lignes'
-SCREEN_LINE_DETECTION = 'Detection de lignes'
+SCREEN_LINE_DETECTION_CONTROL = 'Controlleur Detection de lignes' + str(ITERATION)
+SCREEN_LINE_DETECTION = 'Detection de lignes' + str(ITERATION)
 globalLineDetectionResult = None
 
-SCREEN_DENOISE_AND_SORT_LINES_CONTROL = 'Controlleur debruiter et trier les lignes'
-SCREEN_DENOISE_AND_SORT_LINES = 'Debruiter et trier les lignes'
+SCREEN_DENOISE_AND_SORT_LINES_CONTROL = 'Controlleur debruiter et trier les lignes' + str(ITERATION)
+SCREEN_DENOISE_AND_SORT_LINES = 'Debruiter et trier les lignes' + str(ITERATION)
 globalDenoiseAndSortLinesResult = None
 
-SCREEN_FIND_INTERSECTIONS_CONTROL = 'Controlleur intersections'
-SCREEN_FIND_INTERSECTIONS = 'Intersections'
+SCREEN_FIND_INTERSECTIONS_CONTROL = 'Controlleur intersections' + str(ITERATION)
+SCREEN_FIND_INTERSECTIONS = 'Intersections' + str(ITERATION)
 globalFindIntersectionsResult = None
 
+SCREEN_ISOLATE_PLACES_CONTROL = 'Controlleur isoler places' + str(ITERATION)
+SCREEN_ISOLATE_PLACES = 'Isoler places' + str(ITERATION)
+globalIsolatePlacesResult = None
+
+SCREEN_FIND_POLYGONS_CONTROL = 'Controlleur trouver polygones' + str(ITERATION)
+SCREEN_FIND_POLYGONS = 'Trouver polygones' + str(ITERATION)
+globalFindPolygonsResult = None
+
+SCREEN_FIND_CARS_CONTROL = 'Controlleur trouver voitures' + str(ITERATION)
+SCREEN_FIND_CARS = 'Trouver voitures' + str(ITERATION)
+globalFindCarsResult = None
 
 images = [
     "1.jpg",
@@ -59,10 +72,11 @@ def image_selectors(*args):
     image = f.ResizeWithAspectRatio(image, width=600)
     imageHsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     update_hsv()
+    find_cars()
 
 def init_image_selectors_control():
     cv2.namedWindow(SCREEN_IMAGE_SELECTOR_CONTROL)
-    cv2.createTrackbar('Image', SCREEN_IMAGE_SELECTOR_CONTROL, 2, len(images)-1, image_selectors)
+    cv2.createTrackbar('Image', SCREEN_IMAGE_SELECTOR_CONTROL, 3, len(images)-1, image_selectors)
 
 def init_hsv_control():
     cv2.namedWindow(SCREEN_HSV_CONTROL)
@@ -89,7 +103,7 @@ def update_hsv(*args):
 
     mask = cv2.inRange(imageHsv, lower, upper)
     binary = cv2.bitwise_and(image, image, mask=mask)
-    _, binary = cv2.threshold(binary, 127, 255, cv2.THRESH_BINARY)
+    _, binary = cv2.threshold(binary, 0, 255, cv2.THRESH_BINARY)
 
     Hori = np.concatenate((image, binary), axis=1) 
     cv2.imshow(SCREEN_HSV, Hori)
@@ -127,10 +141,10 @@ def update_opening(*args):
 def init_line_detection_control():
     cv2.namedWindow(SCREEN_LINE_DETECTION_CONTROL)
     cv2.namedWindow(SCREEN_LINE_DETECTION)
-    cv2.createTrackbar('Ouverture Canny', SCREEN_LINE_DETECTION_CONTROL, 2, 2, line_detection)
-    cv2.createTrackbar('Seuil Hough', SCREEN_LINE_DETECTION_CONTROL, 45, 255, line_detection)
-    cv2.createTrackbar('Longueur Min Ligne', SCREEN_LINE_DETECTION_CONTROL, 0, 300, line_detection)
-    cv2.createTrackbar('Ecart Max Ligne', SCREEN_LINE_DETECTION_CONTROL, 60, 100, line_detection)
+    cv2.createTrackbar('Ouverture Canny', SCREEN_LINE_DETECTION_CONTROL, 0, 2, line_detection)
+    cv2.createTrackbar('Seuil Hough', SCREEN_LINE_DETECTION_CONTROL, 57, 255, line_detection)
+    cv2.createTrackbar('Longueur Min Ligne', SCREEN_LINE_DETECTION_CONTROL, 104, 300, line_detection)
+    cv2.createTrackbar('Ecart Max Ligne', SCREEN_LINE_DETECTION_CONTROL, 61, 100, line_detection)
 
 def line_detection(*args):
     global globalOpeningResult
@@ -159,8 +173,8 @@ def line_detection(*args):
 def init_denoise_and_sort_lines():
     cv2.namedWindow(SCREEN_DENOISE_AND_SORT_LINES_CONTROL)
     cv2.namedWindow(SCREEN_DENOISE_AND_SORT_LINES)
-    cv2.createTrackbar('Distinct orientations', SCREEN_DENOISE_AND_SORT_LINES_CONTROL, 5, 6, denoise_and_sort_lines)
-    cv2.createTrackbar('Angle tolerence', SCREEN_DENOISE_AND_SORT_LINES_CONTROL, 90, 100, denoise_and_sort_lines)
+    cv2.createTrackbar('Min batch size', SCREEN_DENOISE_AND_SORT_LINES_CONTROL, 1, 6, denoise_and_sort_lines)
+    cv2.createTrackbar('Angle tolerence', SCREEN_DENOISE_AND_SORT_LINES_CONTROL, 100, 100, denoise_and_sort_lines)
 
 def denoise_and_sort_lines(*args):
     global globalLineDetectionResult
@@ -169,7 +183,7 @@ def denoise_and_sort_lines(*args):
     if not initialized: return
     lines = globalLineDetectionResult.copy()
     imageCopy = image.copy()
-    distinctOrientations = cv2.getTrackbarPos('Distinct orientations', SCREEN_DENOISE_AND_SORT_LINES_CONTROL)
+    distinctOrientations = cv2.getTrackbarPos('Min batch size', SCREEN_DENOISE_AND_SORT_LINES_CONTROL)
     angleTolerence = cv2.getTrackbarPos('Angle tolerence', SCREEN_DENOISE_AND_SORT_LINES_CONTROL)
 
     # Calculer les angles des lignes
@@ -206,7 +220,8 @@ def denoise_and_sort_lines(*args):
     
     globalDenoiseAndSortLinesResult = filteredLines
     cv2.imshow(SCREEN_DENOISE_AND_SORT_LINES, imageCopy)
-    find_intersections()
+    #find_intersections()
+    isolate_places()
 
 def init_find_intersections_control():
     cv2.namedWindow(SCREEN_FIND_INTERSECTIONS_CONTROL)
@@ -249,6 +264,151 @@ def find_intersections(*args):
     cv2.imshow(SCREEN_FIND_INTERSECTIONS, imageCopy)
     globalFindIntersectionsResult = intersections
 
+def init_isolate_places_control():
+    cv2.namedWindow(SCREEN_ISOLATE_PLACES_CONTROL)
+    cv2.namedWindow(SCREEN_ISOLATE_PLACES)
+    cv2.createTrackbar('Largeur segments', SCREEN_ISOLATE_PLACES_CONTROL, 73, 100, isolate_places)
+    cv2.createTrackbar('Erosion', SCREEN_ISOLATE_PLACES_CONTROL, 20, 20, isolate_places)
+    cv2.createTrackbar('Dilatation', SCREEN_ISOLATE_PLACES_CONTROL, 20, 20, isolate_places)
+
+def isolate_places(*args):
+    global globalDenoiseAndSortLinesResult
+    global globalIsolatePlacesResult
+    if not initialized: return
+
+    blackImage = np.zeros(image.shape, np.uint8)
+
+    largeurSegments = cv2.getTrackbarPos('Largeur segments', SCREEN_ISOLATE_PLACES_CONTROL)
+    erosionIterations = cv2.getTrackbarPos('Erosion', SCREEN_ISOLATE_PLACES_CONTROL)
+    dilationIterations = cv2.getTrackbarPos('Dilatation', SCREEN_ISOLATE_PLACES_CONTROL)
+
+    
+    clusters = globalDenoiseAndSortLinesResult.copy()
+    for cluster in clusters:
+        for line in cluster:
+            f.dessiner_rectangle_centre(blackImage, line[0], largeurSegments, (255, 255, 255))
+    
+    for cluster in clusters:
+        for line in cluster:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(blackImage, (x1, y1), (x2, y2), (0, 0, 0), 5)
+
+    # Apply erosion
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    erosion = cv2.erode(blackImage, kernel, iterations=erosionIterations)
+
+    # Apply dilation
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    dilation = cv2.dilate(erosion, kernel, iterations=dilationIterations)
+
+    # Negate images
+    dilation = cv2.bitwise_not(dilation)
+    globalIsolatePlacesResult = dilation
+
+    # Apply AND operation to original image
+    dilation = cv2.bitwise_and(dilation, image)
+    
+    Hori = np.concatenate((blackImage, erosion, dilation), axis=1)
+    cv2.imshow(SCREEN_ISOLATE_PLACES, Hori)
+    find_polygons()
+
+
+def init_find_polygons_control():
+    cv2.namedWindow(SCREEN_FIND_POLYGONS_CONTROL)
+    cv2.namedWindow(SCREEN_FIND_POLYGONS)
+    cv2.createTrackbar('eps', SCREEN_FIND_POLYGONS_CONTROL, 500, 1000, find_polygons)
+    cv2.createTrackbar('min_samples', SCREEN_FIND_POLYGONS_CONTROL, 4, 20, find_polygons)
+
+def find_polygons(*args):
+    global globalIsolatePlacesResult
+    global globalFindPolygonsResult
+    if not initialized: return
+
+    polygons = []
+    eps = cv2.getTrackbarPos('eps', SCREEN_FIND_POLYGONS_CONTROL)
+    min_samples = cv2.getTrackbarPos('min_samples', SCREEN_FIND_POLYGONS_CONTROL)
+    outputImage = image.copy()
+
+    
+    # Convertir en niveaux de gris
+    gray = cv2.cvtColor(globalIsolatePlacesResult, cv2.COLOR_BGR2GRAY)
+
+    # Appliquer un seuillage binaire si nécessaire
+    _, binary = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
+
+    # Trouver les contours
+    contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Calculer la surface de chaque polygone
+    surfaces = np.array([cv2.contourArea(cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)) for contour in contours]).reshape(-1, 1)
+
+    # Appliquer DBSCAN pour identifier les outliers
+    dbscan = DBSCAN(eps=float(eps+0.001), min_samples=min_samples+1)  # Ajustez 'eps' et 'min_samples' au besoin
+    clusters = dbscan.fit_predict(surfaces)
+
+    # Filtrer les polygones (garder ceux qui ne sont pas des outliers)
+    sortedPolygons = [contours[i] for i in range(len(contours)) if clusters[i] != -1]
+
+    # Dessiner les polygones filtrés sur l'image
+    for polygone in sortedPolygons:
+        cv2.drawContours(outputImage, [polygone], 0, (255, 0, 0), 2)
+    
+    globalFindPolygonsResult = sortedPolygons
+    
+    cv2.imshow(SCREEN_FIND_POLYGONS, outputImage)
+
+def init_find_cars_control():
+    cv2.namedWindow(SCREEN_FIND_CARS_CONTROL)
+    cv2.namedWindow(SCREEN_FIND_CARS)
+    #cv2.createTrackbar('Hue Bas', SCREEN_FIND_CARS_CONTROL, 0, 255, find_cars)
+    #cv2.createTrackbar('Saturation Bas', SCREEN_FIND_CARS_CONTROL, 0, 255, find_cars)
+    cv2.createTrackbar('Value Bas', SCREEN_FIND_CARS_CONTROL, 114, 255, find_cars)
+    cv2.createTrackbar('Hue Haut', SCREEN_FIND_CARS_CONTROL, 71, 255, find_cars)
+    cv2.createTrackbar('Saturation Haut', SCREEN_FIND_CARS_CONTROL, 85, 255, find_cars)
+    cv2.createTrackbar('Value Haut', SCREEN_FIND_CARS_CONTROL, 196, 255, find_cars)
+    cv2.createTrackbar('Erosion', SCREEN_FIND_CARS_CONTROL, 1, 20, find_cars)
+    cv2.createTrackbar('Dilatation', SCREEN_FIND_CARS_CONTROL, 1, 20, find_cars)
+
+def find_cars(*args):
+    global globalFindPolygonsResult
+    global globalFindCarsResult
+    if not initialized: return
+    
+    lower_hue = 0 # cv2.getTrackbarPos('Hue Bas', SCREEN_FIND_CARS_CONTROL)
+    lower_saturation = 0 # cv2.getTrackbarPos('Saturation Bas', SCREEN_FIND_CARS_CONTROL)
+    lower_value = cv2.getTrackbarPos('Value Bas', SCREEN_FIND_CARS_CONTROL)
+    upper_hue = cv2.getTrackbarPos('Hue Haut', SCREEN_FIND_CARS_CONTROL)
+    upper_saturation = cv2.getTrackbarPos('Saturation Haut', SCREEN_FIND_CARS_CONTROL)
+    upper_value = cv2.getTrackbarPos('Value Haut', SCREEN_FIND_CARS_CONTROL)
+
+    srcHsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    lower = np.array([lower_hue, lower_saturation, lower_value])
+    upper = np.array([upper_hue, upper_saturation, upper_value])
+    mask = cv2.inRange(srcHsv, lower, upper)
+    binary = cv2.bitwise_and(image, image, mask=mask)
+    _, binary = cv2.threshold(binary, 0, 255, cv2.THRESH_BINARY)
+
+    
+    # Negate binary image
+    binary = cv2.bitwise_not(binary)
+
+    # Apply erosion
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    erosionIterations = cv2.getTrackbarPos('Erosion', SCREEN_FIND_CARS_CONTROL)
+    erosion = cv2.erode(binary, kernel, iterations=erosionIterations)
+
+    # Apply dilation
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    dilationIterations = cv2.getTrackbarPos('Dilatation', SCREEN_FIND_CARS_CONTROL)
+    dilation = cv2.dilate(erosion, kernel, iterations=dilationIterations)
+
+    Hori = np.concatenate((binary, erosion, dilation), axis=1)
+    cv2.imshow(SCREEN_FIND_CARS, Hori)
+
+    globalFindCarsResult = dilation
+
+
 
 
 
@@ -258,10 +418,14 @@ def first_run():
     init_opening_control()
     init_line_detection_control()
     init_denoise_and_sort_lines()
-    init_find_intersections_control()
+    #init_find_intersections_control()
+    init_isolate_places_control()
+    init_find_polygons_control()
+    init_find_cars_control()
     global initialized
     initialized = True
     image_selectors()
+    find_cars()
 
 
 first_run()
